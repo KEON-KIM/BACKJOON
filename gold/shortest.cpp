44460686f24afa4cc9c8bf618a53fbd0fc056122
keon-kim <srihweb@naver.com>
2021-10-20 16:46:53 +0900

    #1753

--- gold/shortest.cpp
+++ gold/shortest.cpp
@@ -1,38 +1,64 @@
+//#1753 < Dijkstr Algorithm Problem>
+//@yabmoons Reference <https://yabmoons.tistory.com/376>
 #include<iostream>
-#include<map>
+#include<queue>
 #include<vector>
-#define MAX 20001;
+
+#define endl "\n"
+#define MAX 20001
+#define INF 987654321
+
 using namespace std;
 
+int Dist[MAX];
+int V, E, Start;
+vector<pair<int, int>> Vertex[MAX];
 
 int main()
 {
-	vector<pair<int,int>> insert_;
-	insert_.push_back(make_pair(0,6));
-	insert_.push_back(make_pair(0,5));
-	insert_.push_back(make_pair(0,1));
-	map<int, int> test;
-	// map<int, int> :: iterator itr;
-	for(int i = 0; i < insert_.size(); i++)
+	ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+	cin >> V >> E;
+	cin >> Start;
+	int a, b, c;
+	for(int i = 0; i < E; i++)
 	{
-		auto itr = test.insert({insert_[i].first, insert_[i].second});
-		if (!itr.second){ // not exist
-		    itr.first ->second = insert_[i].second;
-		}
-		else
+		cin >> a >> b >> c;
+		Vertex[a].push_back(make_pair(b,c));
+	}
+	// Dist init
+	for(int i = 1; i <= V; i++) Dist[i] = INF; 
+
+	priority_queue<pair<int,int>> PQ;
+	PQ.push(make_pair(0, Start));
+	Dist[Start] = 0;
+	cout << Dist[1] << endl;
+	while(!PQ.empty())
+	{
+		int Cost = -PQ.top().first;
+		int Cursor = PQ.top().second;
+		PQ.pop();
+
+		for(int i = 0; i < Vertex[Cursor].size(); i++)
 		{
-			if(itr.second > insert_[i].second)
-				itr.second = insert_[i].second;
+			int next_ = Vertex[Cursor][i].first;
+			int cost_ = Vertex[Cursor][i].second;
+
+			if(Dist[next_] > Cost + cost_)
+			{
+				Dist[next_] = cost_+ Cost;
+				PQ.push(make_pair(-Dist[next_], next_));
+			}
 		}
 	}
-	
-	
-	test.insert({0,2});
-	test.insert({0,3});
-	test.insert({0,4});
-	
-	
-	// iter = test.begin();
-	cout << test.find(0)->second << endl;
+	//print
+	for(int i = 1; i <= V; i++)
+	{
+		if(Dist[i] == INF) cout << "INF" << endl;
+		else cout << Dist[i] << endl;
+	}
+
 	return 0;
 }
