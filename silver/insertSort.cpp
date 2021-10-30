3f7726c6bedbbdbbf08413723aaf1b580ca997e6
keon-kim <srihweb@naver.com>
2021-10-30 18:57:49 +0900

    Study/InsertSort

---
+++ silver/insertSort.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+
+using namespace std;
+
+int N;
+int arr[1001];
+
+void shift(int index){
+	arr[index+1] = arr[index];
+}
+
+int main()
+{
+	int k, i, j;
+	cin >> N;
+	for(int i = 0; i < N; i++)
+		cin >> arr[i];
+
+	for(int i = 1; i < N; i++)
+	{
+		k = arr[i];
+		for(j = i-1; j >= 0 && arr[j] > k; j--)
+			arr[j+1] = arr[j];
+		
+
+		arr[j+1] = k;
+	}
+
+	for(int i = 0; i < N; i++)
+		cout << arr[i] << " ";
+	cout << endl;
+	return 0;
+}
