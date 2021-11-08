dc8e65d4210a5f6d59ed3377c3c1b2415483d262
keon-kim <srihweb@naver.com>
2021-11-08 17:50:13 +0900

    #1932

---
+++ silver/meetingRoom.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int N; 
+vector<pair<int, int>> timeboard;
+
+int main()
+{
+	int a, b, endTime, answer = 1;
+	cin >> N;
+	for(int i = 0; i < N; i++)
+	{
+		cin >> a >> b;
+		timeboard.push_back(make_pair(b, a));
+	}
+
+	sort(timeboard.begin(), timeboard.end());
+
+	endTime = timeboard[0].first;
+	for(int i = 1; i < N; i++)
+	{
+		if(timeboard[i].second >= endTime){
+			answer++;
+			endTime = timeboard[i].first;
+		}
+	}
+
+	cout << answer << endl;
+
+	return 0;
+
+}
+
