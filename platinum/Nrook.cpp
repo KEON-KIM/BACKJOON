a690fe0d2cdd29b492ff4346922af8a9529605a3
keon-kim <srihweb@naver.com>
2021-11-22 14:51:29 +0900

    new folder

---
+++ platinum/Nrook.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+typedef long long ll;
+int n, m, k, mod = 1000001;
+ll dp[101][101][101];
+ 
+ll go(int n, int m, int k) {
+    if (n < 0 || m < 0 || k < 0) return 0;
+    if (k == 0) return 1;
+    ll& ret = dp[n][m][k];
+    if (ret != -1) return ret;
+    ret = 0;
+    ret += go(n - 1, m, k);
+    ret += go(n - 1, m - 1, k - 1) * m;
+    // ret += go(n - 1, m - 2, k - 2) * m * (m - 1) / 2;
+    // ret += go(n - 2, m - 1, k - 2) * m * (n - 1);
+    ret %= mod;
+    return ret;
+}
+ 
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    cin >> n >> k;
+    memset(dp, -1, sizeof(dp));
+    cout << go(n, n, k) << '\n';
+}
