f64fb949c4f53aefb584fba1a6a14fb171aa6cc3
keon-kim <srihweb@naver.com>
2022-02-12 19:38:04 +0900

    #DataStructure

--- silver/priortyQue.cpp
+++ silver/priortyQue.cpp
@@ -1,99 +1,124 @@
 #include <iostream>
-#define MAX_SIZE 100001
-
 using namespace std;
-int n;
 
+#define HEAP_SIZE 256
+#define ARRAY_SIZE 10
 
-void swap(int &a, int &b)
-{
-	int tmp;
-	tmp = a;
-	a = b;
-	b = tmp;
-}
+struct Heap{
+	bool isDesc = false;
+	int heap[HEAP_SIZE];
+	int heap_count = 0;
 
-struct priortyQue
-{
-	int board[MAX_SIZE] = {'\0', };
-	int size = 1;
+	void swap(int* a, int* b) {
+		int tmp = *a; *a = *b; *b = tmp;
+	}
 
-	void push(int input)
-	{
-		
-		int parent_idx = size/2;
-		int cusor = size;
-		board[cusor] = input;
-		
-		while(parent_idx)
+	void init() {
+		heap_count = 0;
+	}
+
+	int size() {
+		return heap_count;
+	}
+
+	void push(int data) {
+		heap[++heap_count] = data;
+
+		int child = heap_count;
+		int parent = child / 2;
+		if(isDesc)
 		{
-			if(board[parent_idx] < board[cusor]){
-				swap(board[parent_idx], board[cusor]);
-				cusor = parent_idx;
+			while (child > 1 && heap[parent] < heap[child]) 
+			{
+				swap(&heap[parent], &heap[child]);
+				child = parent;
+				parent = child / 2;
 			}
-			parent_idx /= 2;
 		}
-		size++;
-	}
-
-	void pop()
-	{
-		if (size == 1) return;
-		int root = 1;
-		int cusor = size-1;
-		int child_idx = root*2;
-		cout << "POP : " << board[root] << endl;
-		board[root] = board[cusor];
-		board[cusor] = '\0';
-		int tmp_index = root;
-		//heapify
-		while(child_idx < size && board[tmp_index] < board[child_idx])
+		else
 		{
-			if(board[child_idx] > board[child_idx+1]){
-				swap(board[tmp_index], board[child_idx]);
-				tmp_index = child_idx;
+			while (child > 1 && heap[parent] > heap[child]) 
+			{
+				swap(&heap[parent], &heap[child]);
+				child = parent;
+				parent = child / 2;
 			}
-			else if(board[child_idx+1] > board[child_idx]){
-				swap(board[tmp_index], board[child_idx+1]);
-				tmp_index = child_idx+1;
+		}	
+	}
+
+	int pop() {
+		int result = heap[1];
+		swap(&heap[1], &heap[heap_count]);
+		heap_count--;
+
+		int parent = 1;
+		int child = parent * 2;
+		if(isDesc)
+		{
+			if (child + 1 <= heap_count) {
+				child = (heap[child] > heap[child + 1]) ? child : child + 1;
 			}
-		
-			child_idx *= 2;
+
+			while (child <= heap_count && heap[parent] < heap[child]) {
+				swap(&heap[parent], &heap[child]);
+
+				parent = child;
+				child = child * 2;
+				if (child + 1 <= heap_count) {
+					child = (heap[child] > heap[child + 1]) ? child : child + 1;
+				}
+			}
 		}
+		else
+		{
+			if (child + 1 <= heap_count) {
+				child = (heap[child] < heap[child + 1]) ? child : child + 1;
+			}
 
-		size --;
+			while (child <= heap_count && heap[parent] > heap[child]) {
+				swap(&heap[parent], &heap[child]);
+
+				parent = child;
+				child = child * 2;
+				if (child + 1 <= heap_count) {
+					child = (heap[child] < heap[child + 1]) ? child : child + 1;
+				}
+			}
+		}
+		return result;
 	}
 
-	void print()
+	int top()
 	{
-		cout << "SIZE : " << size-1 << endl;
-		for(int i = 1; i < size; i++)
-			cout << board[i] << " ";
-		cout << endl;
+		return heap[1];
 	}
 
+	Heap(bool isdesc)
+	{
+		isDesc = isdesc;
+	}
 };
 
-int main()
-{
-	// cin >> n;
-	priortyQue PQ = priortyQue();
-	PQ.push(1);
-	PQ.push(2);
-	PQ.push(-1);
-	PQ.push(33);
-	PQ.push(23);
-	PQ.push(-21);
-	PQ.print();
-	PQ.pop();
-	PQ.pop();
-	PQ.pop();
-	PQ.push(0);
-	// PQ.print();
-	PQ.pop();
-	PQ.print();
-	PQ.pop();
-	PQ.pop();
-	PQ.pop();
+int main() {
+	Heap ascHeap = Heap(false);
+	Heap descHeap = Heap(true);
+	ascHeap.push(1);
+	ascHeap.push(2);
+	ascHeap.push(3);
+	ascHeap.push(4);
+	ascHeap.push(5);
+	ascHeap.pop();
+
+	descHeap.push(1);
+	descHeap.push(2);
+	descHeap.push(3);
+	descHeap.push(4);
+	descHeap.push(5);
+	descHeap.pop();
+	descHeap.pop();
+
+	cout << ascHeap.top() << endl;
+	cout << descHeap.top() << endl;
+
 	return 0;
 }
