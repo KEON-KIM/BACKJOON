d5c8f60a108472e45bd5cab9428d5ae46968fc4a
keon-kim <srihweb@naver.com>
2022-04-27 16:54:23 +0900

    #12869

---
+++ gold/Mutalisk.cpp
@@ -0,0 +1,49 @@
+//#12869
+//@Reference Nimgnoej <https://nim-code.tistory.com/138>
+#include <iostream>
+#include <cstring>
+
+#define INF 987654321
+#define FOR(i, n) for(int i = 0; i < n; i++)
+using namespace std;
+
+int N;
+int DP[61][61][61];
+int scv[3];
+
+int solution(int x, int y, int z) {
+	if (x < 0) return solution(0, y, z);
+	if (y < 0) return solution(x, 0, z);
+	if (z < 0) return solution(x, y, 0);
+	
+	if (x == 0 && y == 0 && z == 0)
+		return 0;
+	
+	int& res = DP[x][y][z];
+	if (res != -1)
+		return res;
+
+	res = INF;
+	res = min(res, solution(x - 9, y - 3, z - 1) + 1);
+	res = min(res, solution(x - 9, y - 1, z - 3) + 1);
+
+	res = min(res, solution(x - 3, y - 9, z - 1) + 1);
+	res = min(res, solution(x - 3, y - 1, z - 9) + 1);
+
+	res = min(res, solution(x - 1, y - 9, z - 3) + 1);
+	res = min(res, solution(x - 1, y - 3, z - 9) + 1);
+
+	return res;
+}
+
+int main() {
+	cin >> N;
+	memset(DP, -1, sizeof(DP));
+	FOR(i, N) {
+		cin >> scv[i];
+	}
+
+	cout << solution(scv[0], scv[1], scv[2]) << '\n';
+	
+	return 0;
+}
