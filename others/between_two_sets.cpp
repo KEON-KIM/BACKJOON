1b62f80db6431a0655a3ad37259613cf3d33a854
keon-kim <srihweb@naver.com>
2022-10-07 16:39:55 +0900

    #practice HackerRank

--- others/between_two_sets.cpp
+++ others/between_two_sets.cpp
@@ -5,13 +5,16 @@
 #define FOR(i, n) for(int i = 0; i < n; i++)
 using namespace std;
 
-int gcd(int a, int b) { 
-    if (b > a) {
+int gcd(int a, int b) 
+{ 
+    if (b > a) //swap
+    {
         int temp = b;
         b = a;
         a = temp;
     }
-    while (b) {
+    while (b) 
+    {
         int c = a % b;
         a = b;
         b = c;
@@ -19,33 +22,43 @@ int gcd(int a, int b) {
     return a;
 }
 
-int lcm(int a, int b) { 
+long long lcm(long long a, long long b) 
+{ 
     return a * b / gcd(a, b);
 }
 
-int getTotalX(int a_count, int* a, int b_count, int* b) {
-    int lcm_a = *a, gcd_b = *b, count = 0;
 
-    for (int i=0;i<a_count && lcm_a<=100;i++) lcm_a = lcm(*(a+i), lcm_a); 
-    for (int i=0;i<b_count;i++) gcd_b = gcd(*(b+i), gcd_b); 
+int solution(vector<int> a, vector<int> b) {
+    long long lcm_a = a[0], gcd_b = b[0], cnt = 0;
+    for (int i=0;i<a.size(); i++) {cout << lcm_a << endl; lcm_a = lcm(a[i], lcm_a); }
+    for (int i=0;i<b.size(); i++) gcd_b = gcd(b[i], gcd_b); 
 
-    if ((gcd_b == 0) || (lcm_a > 100) || (lcm_a > gcd_b)) return 0;
-
+    if ((gcd_b == 0) || (lcm_a >= MAX) || (lcm_a > gcd_b)) return 0;
     for (int i=lcm_a;i<=gcd_b;i++) 
-        if ((gcd_b%i == 0) && (i%lcm_a == 0)) count++;
+        if ((gcd_b%i == 0) && (i%lcm_a == 0)) 
+        	cnt++;
     
-    return count;
+    return cnt;
 }
 
 int main()
 {
-	int N, M, a[MAX], b[MAX];
+	int N, M, input;
+	vector<int> a, b;
+
 	cin >> N >> M;
 	FOR(i, N)
-		cin >> a[i];
+	{
+		cin >> input;
+		a.push_back(input);
+	}
+
 	FOR(i, M)
-		cin >> b[i];
-	cout << solution(N, M, a, b);
+	{
+		cin >> input;
+		b.push_back(input);
+	}
+	cout << solution(a, b);
 
 	return 0;
 }
